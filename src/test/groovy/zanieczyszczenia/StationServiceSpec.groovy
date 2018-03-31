5877301539a11cdae5f272f7d2ce498f0af2399f
Jacek <jace3k@gmail.com>
2018-03-31 17:57:58 +0200

    okok

---
+++ src/test/groovy/zanieczyszczenia/StationServiceSpec.groovy
@@ -0,0 +1,18 @@
+package zanieczyszczenia
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class StationServiceSpec extends Specification implements ServiceUnitTest<StationService>{
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
