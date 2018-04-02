8a8524bd8250271a2a38d6dcb2e7e5cb7f9556e8
Jacek <jace3k@gmail.com>
2018-04-02 03:04:20 +0200

    station

---
+++ grails-app/domain/zanieczyszczenia/Station.groovy
@@ -0,0 +1,19 @@
+package zanieczyszczenia
+
+class Station {
+    int stationId
+    String stationName
+    double gegrLat
+    double gegrLon
+
+    int cityId
+    String cityName
+    String cityCommuneName
+    String cityDistrictName
+    String cityProvinceName
+
+    String addressStreet
+
+    static constraints = {
+    }
+}
